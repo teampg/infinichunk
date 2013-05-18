ed2bdb163cf7edee82d8a2b7fb10bff236078f9b
Jackson Williams <jackson.r.williams@gmail.com>
2013-05-18 13:13:56 -0700

    Fixed bug where rand-generator sometimes went outside range

--- src/logic/generator/SteppedNoiseGradient.java
+++ src/logic/generator/SteppedNoiseGradient.java
@@ -1,5 +1,9 @@
 package logic.generator;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.collect.BoundType;
+
 import teampg.datatypes.valuerange.ValueRangeMapper;
 import teampg.grid2d.point.Pos2D;
 import libnoiseforjava.module.ModuleBase;
@@ -11,11 +15,25 @@ public class SteppedNoiseGradient<T> {
 	private final ValueRangeMapper<Double, T> stepper;
 
 	public SteppedNoiseGradient(ModuleBase input, ValueRangeMapper<Double, T> stepper) {
+		checkArgument(stepper.getBounds().hasUpperBound());
+		checkArgument(stepper.getBounds().upperBoundType() == BoundType.CLOSED);
+		checkArgument(stepper.getBounds().hasLowerBound());
+		checkArgument(stepper.getBounds().lowerBoundType() == BoundType.CLOSED);
+
 		this.input = input;
 		this.stepper = stepper;
 	}
 
 	public T getValue(Pos2D forPos) {
-		return stepper.getValue(input.getValue(forPos.x * NOISE_FACTOR, forPos.y * NOISE_FACTOR, 0));
+		double noiseAtPos = input.getValue(forPos.x * NOISE_FACTOR, forPos.y * NOISE_FACTOR, 0);
+
+		// random noise modules not guaranteed to always fit exactly into range, so coerce them
+		if (noiseAtPos > stepper.getBounds().upperEndpoint()) {
+			noiseAtPos = stepper.getBounds().upperEndpoint();
+		} else if (noiseAtPos < stepper.getBounds().lowerEndpoint()) {
+			noiseAtPos = stepper.getBounds().lowerEndpoint();
+		}
+
+		return stepper.getValue(noiseAtPos);
 	}
 }
